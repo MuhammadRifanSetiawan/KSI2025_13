f8370ca0d07a9a8eda996b8db17b5afe45829619
MuhammadRifanSetiawan <setiawanrigan6@gmail.com>
2025-12-12 19:35:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $payload = file_get_contents("php://input");
+    file_put_contents("webhook.log", $payload . "\n\n", FILE_APPEND);
+    http_response_code(200);
+    echo "Webhook received";
+    exit;
+}
+
+echo "Hello from server!";
 echo "Muhammad Agus Rizal";
 echo "M. Rifan Setiawan";
 echo "Nabila Eka Putri";
 echo "Nova Cahyani Ms";
-?>
 
